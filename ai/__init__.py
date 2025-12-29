cc88f62805595ec76582c8490f1fe7650ddc8aee
atsushi196323 <soccer.player.f30.0613@gmail.com>
2025-12-29 14:39:36 +0900

    Rename folder to .github

--- ai/__init__.py
+++
@@ -1,7 +0,0 @@
-from ai.ai_strategy import AIStrategy
-from ai.random_ai import RandomAI
-from ai.minimax_ai import MinimaxAI
-from ai.stronger_ai import StrongerAI
-from ai.world_class_ai import WorldAI
-
-__all__ = ["AIStrategy", "RandomAI", "MinimaxAI", "StrongerAI", "WorldAI"]
